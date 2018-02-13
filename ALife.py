70148507988064c00210711573bc53ff981adefe
jesse <jesse.read@gmail.com>
2018-02-13 11:36:57 +0100

    change defaults

--- ALife.py
+++ ALife.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python
+
 import sys
 sys.path.append("alife")
 
+# Default map
 map_file = "dat/maps/map_islands2.dat"
 if len(sys.argv) > 1:
     map_file = sys.argv[1]
 
+# Density of initial sprites
 init_sprites = 0
 if len(sys.argv) > 2:
     init_sprites = int(sys.argv[2])
