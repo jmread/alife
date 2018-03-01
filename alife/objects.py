e0e457c299f2a3974e97f025e0b9f1c53c46ba3e
jesse <jesse.read@gmail.com>
2018-03-01 12:18:21 +0100

    bugs now push plants

--- alife/objects.py
+++ alife/objects.py
@@ -33,10 +33,12 @@ action_space = BugSpace(array([-pi/4., -10.]), array([pi/4.,10.]))
 
 # Some constants
 TERRAIN_DAMAGE = 2.  # Added factor when hitting a wall or landing on water
-BOUNCE_DAMAGE = 2.  # Multiplied factor when hitting a friend
+BOUNCE_DAMAGE = 2.   # Multiplied factor when hitting a friend
 FLIGHT_SPEED = 5.    # After this speed, a creature takes flight
 FLIGHT_BOOST = 3.    # Speed is multiplied by this factor if in flight 
-DIVIDE_LIMIT = 2.0   # Divide when at this proportion of energy
+DIVIDE_LIMIT = 2.    # Divide when at this proportion of energy
+BITE_RATIO = 0.6     # What proportion creatuer's size it can bite of another creature
+MAX_BITE = 10.
 
 def burn(angle,speed,size):
     '''
@@ -53,7 +55,7 @@ class Thing(pygame.sprite.DirtySprite):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.ID = ID
         self.radius = 3 + int(math.sqrt(mass/math.pi))
-        self.energy = mass # ~ body mass
+        self.energy = mass
         self.pos = pos
         self.dirty = True
         self.rect, self.image = build_image(self.pos,self.radius,self.ID)
@@ -82,28 +84,24 @@ class Thing(pygame.sprite.DirtySprite):
             self.kill()
             return
 
-        # Wrap
-        self.wrap(world)
-
         while self.dirty:
             # Check for overlap with objects / terrain (if we are a rock or plant; else it is handled elsewhere)
-            color,collision_obj,terrain_centre = world.check_collisions_p(self.pos,self.radius,self)
+            color,collision_obj,terrain_centre = world.check_collisions(self.pos,self.radius,self)
             if terrain_centre is not None:
                 # overlap with terrain
-                self.kill()
                 self.dirty = False
-                #print("Something fell into the water and disappeared")
-                #TODO: SlideOff(self, terrain_centre, 5.)
+                self.kill()
             if collision_obj is not None:
                 if collision_obj.ID == ID_ROCK or collision_obj.ID == ID_PLANT:
-                    # Overlap with rock or tree
-                    SlideApart(self, collision_obj)
+                    # Overlap with rock or plant
+                    slide_apart(self, collision_obj)
                     collision_obj.dirty = True
-                    # TODO: Combine or break apart into two objects ?
                 else:
                     self.dirty = False
             else:
                 self.dirty = False
+            # Wrap
+            self.wrap(world)
         return
 
     def hit_by(self, creature):
@@ -111,18 +109,21 @@ class Thing(pygame.sprite.DirtySprite):
             A creature collides with me
         '''
         if self.ID == ID_PLANT and creature.ID == ID_ANIMAL:
-            # I am a plant and the creature is designed to eat me, can take a bite relative to its size
-            bite = self.radius * 0.6 # bite size
-            creature.energy = creature.energy + bite  * 0.5 #digestion efficiency 
-            self.energy = self.energy - bite     # 
+            # The creature can eat me (one bite at a time, relative to its own size)
+            bite = random.rand() * MAX_BITE
+            creature.energy = creature.energy + bite  * BITE_RATIO
+            self.energy = self.energy - bite
 
         elif self.ID == ID_ROCK:
             # I am a rock
-            creature.energy = creature.energy * 0.95
+            speed = norm(creature.velocity)
+            creature.energy = creature.energy - speed * BOUNCE_DAMAGE         # Ouch!
             self.pos = self.pos + creature.velocity
-            #Reflect(creature)
-            Slide(creature,self.pos)
             self.dirty = True
+
+        # I am bumped away
+        #slide_off(creature,self.pos)
+        slide_apart(creature,self)
 
     def kill(self): # necessary?
         pygame.sprite.Sprite.kill(self)
@@ -192,25 +193,26 @@ class Creature(Thing):
                 # I am the prey!
                 predator = being
                 prey = self
-            #Fight between prey and predator
+            # Fight between prey and predator
             speed_of_attack = norm(predator.velocity)
             if angle_of_attack(predator,prey) > pi/3.:
-                # Attacker has wrong angle of attack (%3.2f > %3.2f)!" % (angle_of_attack(predator,prey), pi/2.)
-                BounceOffFrom(predator,prey)
+                # Attacker has wrong angle of attack
+                # (probably it is the prey bumping into it)
+                slide_apart(predator,prey)
                 predator.energy = predator.energy - speed_of_attack * BOUNCE_DAMAGE   # Ouch!
                 prey.energy = prey.energy - speed_of_attack * BOUNCE_DAMAGE         # Ouch!
             else:
-                bite = speed_of_attack * random.rand() * 10.
-                # Attacker successful (bites off %3.2f cal). % bite
+                # Attacker successful
+                bite = speed_of_attack * random.rand() * MAX_BITE
                 predator.energy = predator.energy + bite
                 prey.energy = prey.energy - bite
-                BounceOffFrom(predator,prey)
+                slide_apart(predator,prey)
 
         else:
             # The being is from the same species (TODO: add reproduction option here)
             self.energy = self.energy - norm(being.velocity) * BOUNCE_DAMAGE   # Ouch!
             being.energy = being.energy - norm(self.velocity) * BOUNCE_DAMAGE  # Ouch!
-            BounceOffFrom(self,being)
+            slide_apart(self,being)
 
     def live(self, world):
 
@@ -227,9 +229,9 @@ class Creature(Thing):
         self.observation = zeros(N_INPUTS, dtype=float)
 
         # Check collisions with terrain, and other objects (TODO: we probably want to do a global collision detection in 'world' before each round)
-        self.observation[IDX_COLIDE],collision_obj,terrain_centre = world.check_collisions_p(self.pos,self.radius*4.,self,rext=self.radius)
-        self.observation[IDX_PROBE1],o1,t1 = world.check_collisions_p(self.pos+self.pa1,self.radius*3.,self)
-        self.observation[IDX_PROBE2],o2,t2 = world.check_collisions_p(self.pos+self.pa2,self.radius*3.,self)
+        self.observation[IDX_COLIDE],collision_obj,terrain_centre = world.check_collisions(self.pos,self.radius*4.,self,collision_radius=self.radius)
+        self.observation[IDX_PROBE1],o1,t1 = world.check_collisions(self.pos+self.pa1,self.radius*3.,self)
+        self.observation[IDX_PROBE2],o2,t2 = world.check_collisions(self.pos+self.pa2,self.radius*3.,self)
 
         # Unless we are flighing, we will collide with any objects we overlap with
         if norm(self.velocity) < FLIGHT_SPEED:
@@ -237,7 +239,7 @@ class Creature(Thing):
             if terrain_centre is not None:
                 ## Terrain/water collision
                 self.energy = self.energy - norm(self.velocity) * TERRAIN_DAMAGE
-                Slide(self,terrain_centre)
+                slide_off(self,terrain_centre)
 
             if collision_obj is not None:
                 # Sprite (rock,plant,bug) collision
