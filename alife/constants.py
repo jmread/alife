a6e5a489baceef71d05f948500a22d815d2e5e44
Jesse Read <jesse.read@gmail.com>
2026-07-08 08:00:28 +0200

    add 3rd action; revise constants

--- alife/constants.py
+++ alife/constants.py
@@ -1,7 +1,6 @@
 # World Parameters
 
 TILE_SIZE = 64               # tile size (width and height, in pixels)
-MAX_GRID_DETECTION = 100     # maximum number of objects that can be detected at once
 DEBUG = False
 
 # Sprite Parameters
@@ -22,6 +21,7 @@ IDX_img = 4                    # int : image id; the 'coat' for a given sprite o
 IDX_sid = 5                    # int : this is a sub-id; 
                                #                if sprite[IDX_id] = ID_FLAG (i.e., a flag/nest) then sprite[IDX_sid] = n for n-th flag
                                #                if sprite[IDX_id] = ID_ANIMAL (i.e., another bug) then sprite[IDX_sid] = Team ID
+
 DISK_INDICES = [IDX_id, IDX_x, IDX_y, IDX_rad, IDX_img, IDX_sid]
 # INTERNAL STATE -- for computations and drawing
 IDX_spear0 = [6,7]             # int : relative position of spear (supposing base at [0,0])
@@ -30,12 +30,14 @@ IDX_anten2 = [10,11]             # int : relative position of right antenna (sup
 IDX_vx = 12                    # float
 IDX_vy = 13                    # float
 IDX_unitv = [IDX_vx, IDX_vy]   # float
-IDX_dangle = 14                # float : unused
-IDX_penergy = 15               # float : unused
+IDX_health = 14                # int : between 0 and MAX_HEALTH
+IDX_speed = 15                 # int : internal state speed 
 IDX_tid = 16                   # int : targit id; points to the id (array index) of the sprite that this sprite should look for/target
-IDX_health = 17                # int : between 0 and MAX_HEALTH
-IDX_damage = 18                # int : for tracking external splatter
-IDX_speed = 19                 # int : internal state speed 
+
+IDX_unused = 17                # float : unused
+IDX_glitter = 18               # float : glitter
+IDX_damage = 19                # int : for tracking external splatter
+
 # OBSERVATION (all between 0 and 1) : FLOAT
 IDX_COLIDE = 20                 # (OBS) between 0 and 1
 IDX_PROXIMITY = 21              # (OBS) between 0 and 1 (nb actually currently can be > 1)
@@ -46,18 +48,17 @@ IDX_FLAG = 29                   # (OBS) how close are we pointing to the flag (1
 IDX_SPEED = 30                  # (OBS) normalised speed
 IDX_OBS = [IDX_COLIDE, IDX_PROXIMITY] + IDX_PROBE1 + IDX_PROBE2 + [IDX_ENERGY,IDX_FLAG,IDX_SPEED]
 d_S = len(IDX_OBS)
+
 ## ACTION (all between -1 and +1)
 IDX_RANGLE = 31                  # (ACT) int : change in angle, in radians, in range [-1, +1]
 IDX_LANGLE = 32                  # (ACT) int : this is more like thrust than speed, in range [-1, +1]
+IDX_SHIFTF = 33                  # (ACT) int : this is more like thrust than speed, in range [-1, +1]
 IDX_ACTIONS = [IDX_RANGLE, IDX_LANGLE]
 d_A = len(IDX_ACTIONS)
+
 ## REWARD
-IDX_RWD = 33
+IDX_RWD = 34
 ## DONE
-IDX_done = 34 
+IDX_done = 35 
 
-IDX_nesti = 36                 # int : unused
-IDX_cid = 37                   # int : unused
-
-D_SPRITE = 38
-N_SPRITE = 50
+D_SPRITE = 36
