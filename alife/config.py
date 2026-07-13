3f61896da6b7517c46ccd7a49fd51b002da28878
Jesse Read <jesse.read@gmail.com>
2026-07-13 20:26:50 +0200

    add defaults for map editor

--- alife/config.py
+++ alife/config.py
@@ -4,7 +4,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 IMG_DIR = os.path.join(ROOT_DIR, 'img')
 MAP_DIR = os.path.join(ROOT_DIR, 'maps')
 
-FPS = 6
+FPS = 10
 MAX_GRID_DETECTION = 100     # maximum number of objects that can be detected at once
 
 # Default map generation dimensions (must be even and >= 4)
