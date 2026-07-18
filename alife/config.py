db6c14c01b662e9d85d3512dc2ce7aaaf06fea75
Jesse Read <jesse.read@gmail.com>
2026-07-18 17:23:20 +0200

    remove unused MAX_GRID_DETECTION variable

--- alife/config.py
+++ alife/config.py
@@ -2,10 +2,10 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 IMG_DIR = os.path.join(ROOT_DIR, 'img')
-MAP_DIR = os.path.join(ROOT_DIR, 'maps')
+MAP_DIR = os.environ.get('ALIFE_MAP_DIR', os.path.join(ROOT_DIR, 'maps'))
 
+# Frames Per Second (in human rendering mode)
 FPS = 10
-MAX_GRID_DETECTION = 100     # maximum number of objects that can be detected at once
 
 # Default map generation dimensions (must be even and >= 4)
 MAP_ROWS = 12
